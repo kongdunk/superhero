389aff11142fe037bb438936b772b83446d33c27
itong6 <itong6@my.bcit.ca>
2023-02-01 19:16:11 -0800

    Add backgrund img and style selection page

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,4 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
---
+++ public/bg.js
@@ -0,0 +1,18 @@
+export const bgImages = [
+    {
+        id: 0,
+        imageUrl: '/bg/0.jpg'
+    },
+    {
+        id: 1,
+        imageUrl: '/bg/1.jpg'
+    },
+    {
+        id: 2,
+        imageUrl: '/bg/2.jpg'
+    },
+    {
+        id: 3,
+        imageUrl: '/bg/3.jpg'
+    },
+]
