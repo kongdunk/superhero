02e0b94c760bcd39f0d5b0ed278b8b4cb0298573
daesankim <62229260+kongdunk@users.noreply.github.com>
2023-03-07 19:22:33 -0800

    Delete onlySelect.js
    deleted because of prerender-error while deploying (moving non-pages out of the pages folder)

--- pages/onlySelect.js
+++
@@ -1,6 +0,0 @@
-export default function onlySelect(id){
-    for (let i = 1; i < 6; i++) {
-      document.getElementById("check" + i).checked = false;
-    }
-    document.getElementById(id).checked = true;
-  }
